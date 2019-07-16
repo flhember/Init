211274dbc2c7fda0a8a947181cda2bbe880fa7b4
flhember <flhember@debian>
2019-07-16 16:39:04 +0200

    script ok

--- scripts/03.sh
+++ scripts/03.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SEARCH=$(zenity --entry --text "Qu'est-ce que vous voulez ecouter?")
-xdg-open "http://www.youtube.com/results?search_query=$SEARCH" 2>dev/null
+xdg-open "http://www.youtube.com/results?search_query=$SEARCH" 2>/dev/null
 exit 0
